713501667709a827917e626e60804f4e5cbf625a
AdrianCiuciui <31804204+AdrianCiuciui@users.noreply.github.com>
2018-08-03 00:11:01 +0300

    Update Main.java
    Added some comments

--- Main.java
+++ Main.java
@@ -5,6 +5,7 @@ public class Main {
     public static void main(String[] args) {
         
         // This part is for testing purposes
+        // Scroll to the methods to see the actual code
         
 //        printMegaBytesAndKiloBytes (0);
 //        if ((bark (true, 8))) {
